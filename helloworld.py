3da95dd1efdf27adb6635bfd9bbda135ade6e309
rnabut <rnabut@umich.edu>
2019-09-09 12:43:07 -0400

    Push New Version

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+# Lab 0
+import random
+
+print("Reidel\n")
+
+x = random.randrange(1, 101, 1)
+
+print(x)
+
+y = random.randrange(1, 101, 1)
+
+print(y)
+
+print("Sume = ", x + y)
+
+print("Average = ", (x+y)/2)
